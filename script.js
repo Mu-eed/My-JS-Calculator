8a17130b443a3e8bab01e5fd740bb6384a6fd8bb
Mu-eed <mueedb036@gmail.com>
2022-11-26 12:30:19 +0200

    im just that guy bruv

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 let sum = "";
 let allowComa = true;
 
+
 function buildSum(char) {
   sum += char;
   document.getElementById("result").innerHTML += char;
 }
 
-// function displaydot(num) {
-//     if (allowComa === true) {
-//         result.value = result.value + num;
-//         allowComa = false
-//     }
-// }
+function buildSumOperators(op) {
+    sum += op;
+    document.getElementById("result").innerHTML += op; 
+    allowComa = true
+}
+
+function buildSumDecimal() {
+    if (allowComa) {
+        sum += '.';
+        document.getElementById("result").innerHTML += '.';
+        allowComa = false   
+    }
+}
+
 
 function calculate() {
   sum = eval(sum);
   document.getElementById("result").innerHTML = sum;
+  if (document.getElementById("result").innerHTML.includes('.')) {
+    allowComa = false
+  } else allowComa = true
 }
 
 function reset() {
   sum = "";
   document.getElementById("result").innerHTML = "";
+  allowComa = true
 }
 
 function del (){
+    if (result.innerText[result.innerText.length-1] == '.') {
+        allowComa = true
+    } else allowComa = false
     result.innerHTML = result.innerHTML.slice(0,-1);
 }
