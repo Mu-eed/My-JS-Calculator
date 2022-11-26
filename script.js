5cb63cecb213b9a0fb0b10a383442ab1e489c09f
Mu-eed <mueedb036@gmail.com>
2022-11-26 11:22:03 +0200

    W rizz

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let sum = "";
+let allowComa = true;
+
 function buildSum(char) {
   sum += char;
   document.getElementById("result").innerHTML += char;
 }
+
+// function displaydot(num) {
+//     if (allowComa === true) {
+//         result.value = result.value + num;
+//         allowComa = false
+//     }
+// }
+
 function calculate() {
   sum = eval(sum);
   document.getElementById("result").innerHTML = sum;
 }
+
 function reset() {
   sum = "";
   document.getElementById("result").innerHTML = "";
+}
+
+function del (){
+    result.innerHTML = result.innerHTML.slice(0,-1);
 }
